88c70ad2ac7ba4140f565d01fdf1716620f6cb32
Akhil Sahdev <akhilsah577@gmail.com>
2021-06-09 06:28:01 +0530

    Change NACL policies

--- nacl.tf
+++ nacl.tf
@@ -8,46 +8,20 @@ resource "aws_network_acl" "private_subnet" {
   subnet_ids = ["${aws_subnet.private.id}"]
 }
 
-resource "aws_network_acl_rule" "private_nacl_ingress_http" {
+resource "aws_network_acl_rule" "public_nacl_ingress" {
   network_acl_id = aws_network_acl.private_subnet.id
-  rule_number    = 300
-  egress         = false
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = var.vpc_cidr
-  from_port      = 80
-  to_port        = 80
-}
-
-resource "aws_network_acl_rule" "private_nacl_egress_all" {
-  network_acl_id = aws_network_acl.private_subnet.id
-  rule_number    = 400
-  egress         = true
-  protocol       = "tcp"
-  rule_action    = "allow"
-  cidr_block     = var.vpc_cidr
-  from_port      = 0
-  to_port        = 0
-}
-
-resource "aws_network_acl_rule" "public_nacl_ingress_http" {
-  network_acl_id = aws_network_acl.public_subnet.id
   rule_number    = 100
   egress         = false
-  protocol       = "tcp"
+  protocol       = "-1"
   rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-  from_port      = 80
-  to_port        = 80
+  cidr_block     = var.vpc_cidr
 }
 
-resource "aws_network_acl_rule" "public_nacl_egress_all" {
-  network_acl_id = aws_network_acl.public_subnet.id
-  rule_number    = 200
+resource "aws_network_acl_rule" "public_nacl_egress" {
+  network_acl_id = aws_network_acl.private_subnet.id
+  rule_number    = 100
   egress         = true
-  protocol       = "tcp"
+  protocol       = "-1"
   rule_action    = "allow"
-  cidr_block     = "0.0.0.0/0"
-  from_port      = 0
-  to_port        = 0
+  cidr_block     = var.vpc_cidr
 }
--- test/unit/nacl.rego
+++ test/unit/nacl.rego
@@ -1,41 +1,12 @@
 package main
 
 deny[msg] {
-  not input.resource.aws_network_acl.public_subnet
-  msg = "Public NACL not defined"
+  not input.resource.aws_network_acl_rule.public_nacl_ingress
+  msg = "Ingress NACL not defined"
 }
 
 deny[msg] {
-  not input.resource.aws_network_acl.private_subnet
-  msg = "Private NACL not defined"
+  not input.resource.aws_network_acl_rule.public_nacl_egress
+  msg = "Public NACL egress rule not defined"
 }
 
-deny[msg] {
-  not input.resource.aws_network_acl_rule.private_nacl_ingress_http
-  msg = "Private NACL http ingress rule not defined"
-}
-
-deny[msg] {
-  not input.resource.aws_network_acl_rule.private_nacl_egress_all
-  msg = "Private NACL outbound rule for all not defined"
-}
-
-deny[msg] {
-  not input.resource.aws_network_acl_rule.public_nacl_ingress_http
-  msg = "Public NACL http ingress rule not defined"
-}
-
-deny[msg] {
-  not input.resource.aws_network_acl_rule.public_nacl_egress_all
-  msg = "Public NACL outbound rule for all not defined"
-}
-
-deny[msg] {
-  not re_match("0.0.0.0/0", input.resource.aws_network_acl_rule.public_nacl_ingress_http.cidr_block)
-  msg = "Public NACL rule from internet not defined"
-}
-
-deny[msg] {
-  not re_match("0.0.0.0/0", input.resource.aws_network_acl_rule.public_nacl_egress_all.cidr_block)
-  msg = "Public NACL outbound rule for all not defined"
-}
